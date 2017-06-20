80499a5c81356a29cf8504e5dc532447cdb26d9e
Jeremy Thurgood <firxen@gmail.com>
2017-06-20 12:16:44 +0200

    Dir already exists.

--- ci/test.sh
+++ ci/test.sh
@@ -7,7 +7,6 @@ bundle install
 bundle exec rspec
 popd
 
-mkdir release
 tar czf release/flight-school-0.1.0.tgz flight-school/
 echo 'flight-school' > release/gh-name
 echo '0.1.0' > release/gh-tag
